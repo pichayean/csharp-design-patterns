38dbca91b3bada8c0d22ad26b56124e07b72c1a3
pichayean <pichayeanyensiri@gmail.com>
2022-05-17 21:05:50 +0700

    first commit & abstract-factory

--- abstract-factory/ProductB1.cs
+++ abstract-factory/ProductB1.cs
@@ -3,7 +3,6 @@ class ProductB1 : AbstractProductB
 {
     public override void Interact(AbstractProductA a)
     {
-        Console.WriteLine(this.GetType().Name +
-          " interacts with " + a.GetType().Name);
+        Console.WriteLine(this.GetType().Name + " interacts with " + a.GetType().Name);
     }
 }
